fa4894cc9e67ee761e1f56bd772bfcaace9951d8
cgperlo <perloguillermo@gmail.com>
2023-04-28 21:06:09 -0300

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
 
+
 function App() {
+  const  [data, setData ] = useState(null);
+  useEffect (() => {
+    
+  }
+  )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fetch like a pro</h1>
+      <div className="card"> </div>
     </div>
   );
 }

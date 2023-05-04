7ff78eac0dc83dfd4b118f71d9214d2df776e536
Gustavo ERNESTO <102641696+GusMaEr@users.noreply.github.com>
2023-05-03 21:42:09 -0300

    20230503_NavBar_BAD

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { RouterBrowser ,Routes, Route} from 'react-router-dom'
+import React from 'react'
+import { Routes, Route} from 'react-router-dom'
 import Home from './Paginas/Home'
 import AcercaDe from './Paginas/AcercaDe'
 import Contacto from './Paginas/Contacto'
@@ -7,15 +8,16 @@ import Navbar from './Componentes/NavBar'
 function App() {
   return (
     <>
-   <Navbar/>
-   
-    <Routes>
-        <Route exact path='/' element={<Home/>} />
-        <Route path='/Acerca de' element={<AcercaDe/>} />
-        <Route path='Contacto' element={<Contacto/>} />
-    </Routes>
+  
+      <Navbar/> 
+      
+      <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route path='/Acerca de' element={<AcercaDe/>} />
+          <Route path='/Contacto' element={<Contacto/>} />
+      </Routes>
     
-     </>
+    </>
   )
 }
 
---
+++ src/Paginas/AcercaDe.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function  AcercaDe  ()  {
+  return (
+    <div>AcercaDe</div>
+  )
+}
+
+export default AcercaDe

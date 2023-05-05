7e8537a6eff4c015349ae919269d179413dd7a4f
Gustavo ERNESTO <102641696+GusMaEr@users.noreply.github.com>
2023-05-05 20:11:56 -0300

    20230505

--- src/Componentes/NavBar.jsx
+++ src/Componentes/NavBar.jsx
@@ -1,30 +1,7 @@
-import {Link,Nav, NavLink} from 'react'
-import { useState } from 'react';
-import {FcHome} from 'react-icons/fc'
-import { FaBars } from 'react-icons/fa'
-import { AiOutlineClose } from 'react-icons/ai'
+import React from 'react'
 
-function Navbar() 
-{
-  const [togglerNav, setTogglernav] = useState(false);
-
-  const clickHandler=() => {setTogglernav(!togglerNav)};
-
+export default function NavBar() {
   return (
-  <Nav className='h-auto md:h-24 p2 bg-blue-200 max-w-6x1 mx-auto flex justify-between md:items-center'>
-    {/* <Link to='/'>
-      <FcHome className='text-6x1'/>
-    </Link> */}
-      <div className={togglerNav ? 'flex  flex-col gap-4 md:inline' : 'hidden md:inline'}>
-        <NavLink className='nav-link' onClick={clickHandler} to='/'>Home</NavLink>
-        <NavLink className='nav-link' onClick={clickHandler} to='/AcercaDe'>Acerca de</NavLink>
-        <NavLink className='nav-link' onClick={clickHandler} to='/Contacto'>Contacto</NavLink>
-      </div>  
-    {/* <button className='inline md:hidden self-start nav-link' onClick={clickHandler}>{togglerNav ? <AiOutlineClose />:<FaBars/> }</button> */}
-  </Nav>
-  
-  );
-
+    <div>NavBar</div>
+  )
 }
-
-export default Navbar;
